899881545d10ac5a1319aea0eb040426dab0a45c
Pablo Veiga <nwpablodeveloper@gmail.com>
2023-12-05 22:37:30 +0100

    Universidad Java

--- src/seccion01/CursoJava.java
+++ src/seccion01/CursoJava.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package cursojava;
+package seccion01;
 
 /**
  *
@@ -14,7 +14,7 @@ public class CursoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola");
     }
     
 }
