16638b0c844880c1d891a0e478a59963b8533718
Pablo Veiga <nwpablodeveloper@gmail.com>
2024-02-10 12:27:36 +0100

    Sección 42 - Manejo de Archivos

---
+++ src/programacion/seccion42/test/TestManejoArchivos.java
@@ -0,0 +1,30 @@
+package programacion.seccion42.test;
+
+import java.io.FileNotFoundException;
+import static programacion.seccion42.manejoarchivos.ManejoArchivos.escribirArchivo;
+import static programacion.seccion42.manejoarchivos.ManejoArchivos.crearArchivo;
+import static programacion.seccion42.manejoarchivos.ManejoArchivos.anexarArchivo;
+import static programacion.seccion42.manejoarchivos.ManejoArchivos.leerArchivo;
+//import static programacion.seccion42.manejoarchivos.ManejoArchivos.*;
+
+public class TestManejoArchivos {
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        var nombreArchivos = "prueba.txt";
+        
+        crearArchivo(nombreArchivos);
+        
+        escribirArchivo(nombreArchivos, "estoy escribiendo el archivo");
+        escribirArchivo(nombreArchivos, "sobreescribi el archivo");
+        
+        anexarArchivo(nombreArchivos, "Estoy agregando contenido");
+        anexarArchivo(nombreArchivos, "Estoy agregando contenido 2");
+        anexarArchivo(nombreArchivos, "Estoy agregando contenido 3");
+        
+        System.out.println("");
+        leerArchivo(nombreArchivos);
+        
+        
+        
+    }
+}
